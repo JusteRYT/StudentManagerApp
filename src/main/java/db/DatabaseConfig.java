d179eaf3b6350bdcb523caaac18db74b2f3e6c0d
Sergey <tohappyoneguy@gmail.com>
2024-11-01 17:05:33 +0300

    Добавлен класс DatabaseConfig для подлкючения к базе данных

---
+++ src/main/java/db/DatabaseConfig.java
@@ -0,0 +1,24 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Класс для управления подключением к базе данных.
+ */
+public class DatabaseConfig {
+    private static final String URL = "jdbc:mysql://localhost:3306/StudentDB";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    /**
+     * Устанавливаем соединение с базой данных
+     *
+     * @return Объект Connection для работы с БД
+     * @throws SQLException если не удалось подключится к БД
+     */
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(URL, USER,PASSWORD);
+    }
+}
